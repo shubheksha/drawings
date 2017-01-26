8e1e610b88b66fddfecca48062d49a2fe1e900e2
Shubheksha Jalan <jshubheksha@gmail.com>
2017-01-26 23:42:00 +0530

    fix rss issue

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://shubheksha.com/drawings',
         mail: {},
         database: {
             client: 'sqlite3',
