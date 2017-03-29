f6faa1b298c1bfcb9d9e72dffc74f91429beb627
Shubheksha Jalan <jshubheksha@gmail.com>
2017-03-29 16:33:31 +0530

    add databases and compile-time vs run-time errors

--- static/shared/ghost-url.js
+++ static/shared/ghost-url.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var apiUrl = '//shubheksha.com/drawings/ghost/api/v0.1/',
+    var apiUrl = '//localhost:2368/ghost/api/v0.1/',
         clientId,
         clientSecret,
         url,
